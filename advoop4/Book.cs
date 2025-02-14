bd95782349b7be0add56dd7b7aaaffd3853ebe18
Sondos-Mahmoud <sondos.mohamed02@eng-st.cu.edu.eg>
2025-02-14 05:50:14 +0200

    adv4

--- advoop4/Book.cs
+++ advoop4/Book.cs
@@ -3,11 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static advoop4.BookFunctions;
 
 namespace advoop4
 {
-    internal class Book
+    public class Book
     {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
 
+        public Book(string _ISBN, string _Title, string[] _Authors, DateTime _PublicationDate, decimal _Price)
+        {
+            ISBN = _ISBN;
+            Title = _Title;
+            Authors = _Authors;
+            PublicationDate = _PublicationDate;
+            Price = _Price;
+        }
+        public override string ToString()
+        {
+            string authors = string.Join(", ", Authors);
+            return $"ISBN: {ISBN}, Title: {Title}, Authors: {authors}, Publication Date: {PublicationDate.ToShortDateString()}, Price: {Price:C}";
+        }
     }
+    public class BookFunctions
+    {
+        public delegate string BookFunctionDelegate(Book B);
+        public static string GetTitle(Book B)
+        {
+            return B.Title;
+        }
+
+        public static string GetAuthors(Book B)
+        {
+            return string.Join(", ", B.Authors);
+        }
+
+        public static string GetPrice(Book B)
+        {
+            return $"{B.Price:C}";
+        }
+    }
+    //public class LibraryEngine
+    //{
+    //    public static void ProcessBooks(List<Book> blist, BookFunctionDelegate fPtr)
+    //    {
+    //        foreach (Book B in blist)
+    //        {
+    //            Console.WriteLine(fPtr(B));
+    //        }
+    //    }
+    //}
+    public class LibraryEngine
+    {
+        public static void ProcessBooks(List<Book> blist, Func<Book, string> fPtr)
+        {
+            foreach (Book B in blist)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+    }
+
 }
--- advoop4/Program.cs
+++ advoop4/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Book> bookList = new List<Book>
+        {
+            new Book("123-456-789", "Book1", new string[] { "Author1" }, new DateTime(2020, 1, 1), 29.99m),
+            new Book("987-654-321", "Book2", new string[] { "Author2", "Author3" }, new DateTime(2021, 2, 2), 39.99m)
+        };
+
+            LibraryEngine.ProcessBooks(bookList, B => B.Title);
+            LibraryEngine.ProcessBooks(bookList, B => string.Join(", ", B.Authors));
+            LibraryEngine.ProcessBooks(bookList, delegate (Book B) { return B.ISBN; });
+            LibraryEngine.ProcessBooks(bookList, B => B.PublicationDate.ToShortDateString());
         }
     }
-}
+    }
